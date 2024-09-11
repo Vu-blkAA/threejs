bddeb908e43d8e97242e1213a2d9c4954b03a72b
Vu-blkaa <vublkaa@gmail.com>
2024-09-11 21:12:04 +0700

    [video5] Complete video 5 with cannon

--- src/js/script.js
+++ src/js/script.js
@@ -17,7 +17,7 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-camera.position.set(0, 15, 15)
+camera.position.set(0, 50, 50);
 
 const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.update();
@@ -25,55 +25,108 @@ orbit.update();
 const planeGeo = new THREE.PlaneGeometry(30, 30);
 const planeMat = new THREE.MeshBasicMaterial({
   wireframe: true,
-  color: 0xFFFFFF,
-  side: THREE.DoubleSide
+  color: 0xffffff,
+  side: THREE.DoubleSide,
 });
 
 const plane = new THREE.Mesh(planeGeo, planeMat);
 
-scene.add(plane)
-
-const world = new CANNON.World({
-  gravity: new CANNON.Vec3(0, -9.81, 0)
-});
-
-const groundBody = new CANNON.Body({
-  shape: new CANNON.Plane(),
-  // mass: 0,
-  type: CANNON.Body.STATIC
-})
-
-groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0)
-
-world.addBody(groundBody);
+scene.add(plane);
 
 const boxGeo = new THREE.BoxGeometry(2, 2, 2);
 const boxMat = new THREE.MeshBasicMaterial({
   wireframe: true,
-  color: 0x00FF00
+  color: 0x00ff00,
 });
 const box = new THREE.Mesh(boxGeo, boxMat);
 
 scene.add(box);
 
+const sphereGeo = new THREE.SphereGeometry(2);
+const sphereMat = new THREE.MeshBasicMaterial({
+  wireframe: true,
+  color: 0xff0000,
+});
+const sphere = new THREE.Mesh(sphereGeo, sphereMat);
+
+scene.add(sphere);
+
+const world = new CANNON.World({
+  gravity: new CANNON.Vec3(0, -9.81, 0),
+});
+
+const groundBodyMat = new CANNON.Material();
+
+const groundBody = new CANNON.Body({
+  // shape: new CANNON.Plane(30, 30),
+  shape: new CANNON.Box(new CANNON.Vec3(15, 15, 0.1)),
+  type: CANNON.Body.STATIC,
+  material: groundBodyMat,
+});
+
+groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+
+world.addBody(groundBody);
+
+const boxBodyMat = new CANNON.Material();
+
 const boxBody = new CANNON.Body({
   shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1)),
   mass: 1,
-  position: new CANNON.Vec3(1, 20, 0)
-})
+  position: new CANNON.Vec3(-5, 20, 0),
+  material: boxBodyMat,
+});
 
-world.addBody(boxBody)
+world.addBody(boxBody);
 
-const timeStep = 1 / 60;
+boxBody.angularVelocity = new CANNON.Vec3(0, 10, 0);
+boxBody.angularDamping = 0.5;
+
+const sphereBodyMat = new CANNON.Material();
+
+const sphereBody = new CANNON.Body({
+  shape: new CANNON.Sphere(2),
+  position: new CANNON.Vec3(2, 2, 0),
+  mass: 1,
+  material: sphereBodyMat,
+});
+
+world.addBody(sphereBody);
+
+sphereBody.linearDamping = 0;
+
+const timeStep = 1 / 120;
+
+const groundBoxContactMat = new CANNON.ContactMaterial(
+  groundBodyMat,
+  boxBodyMat,
+  {
+    friction: 0.04,
+  }
+);
+
+const groundSphereContactMat = new CANNON.ContactMaterial(
+  groundBodyMat,
+  sphereBodyMat,
+  {
+    restitution: 2,
+  }
+);
+
+world.addContactMaterial(groundBoxContactMat);
+world.addContactMaterial(groundSphereContactMat);
 
 function animate() {
   world.step(timeStep);
 
-  plane.position.copy(groundBody.position)
-  plane.quaternion.copy(groundBody.quaternion)
+  plane.position.copy(groundBody.position);
+  plane.quaternion.copy(groundBody.quaternion);
 
-  box.position.copy(boxBody.position)
-  box.quaternion.copy(boxBody.quaternion)
+  box.position.copy(boxBody.position);
+  box.quaternion.copy(boxBody.quaternion);
+
+  sphere.position.copy(sphereBody.position);
+  sphere.quaternion.copy(sphereBody.quaternion);
 
   renderer.render(scene, camera);
 }
