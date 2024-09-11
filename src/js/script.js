230fcebbe60ffa927a4ea4b304e9ee98d8ea2961
Vu Luong <vul@Liberty-MacBook-Pro-M1.local>
2024-09-11 20:28:34 +0700

    [video] Video 5

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import * as CANNON from "cannon-es";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 import stars from "../image/stars.jpg";
@@ -16,25 +17,63 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-camera.position.set(1, 0, 0)
+camera.position.set(0, 15, 15)
 
 const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.update();
 
-const gridHelper = new THREE.GridHelper(30);
-scene.add(gridHelper);
+const planeGeo = new THREE.PlaneGeometry(30, 30);
+const planeMat = new THREE.MeshBasicMaterial({
+  wireframe: true,
+  color: 0xFFFFFF,
+  side: THREE.DoubleSide
+});
 
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-scene.background = cubeTextureLoader.load([
-  stars,
-  stars,
-  stars,
-  stars,
-  stars,
-  stars,
-]);
+const plane = new THREE.Mesh(planeGeo, planeMat);
+
+scene.add(plane)
+
+const world = new CANNON.World({
+  gravity: new CANNON.Vec3(0, -9.81, 0)
+});
+
+const groundBody = new CANNON.Body({
+  shape: new CANNON.Plane(),
+  // mass: 0,
+  type: CANNON.Body.STATIC
+})
+
+groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0)
+
+world.addBody(groundBody);
+
+const boxGeo = new THREE.BoxGeometry(2, 2, 2);
+const boxMat = new THREE.MeshBasicMaterial({
+  wireframe: true,
+  color: 0x00FF00
+});
+const box = new THREE.Mesh(boxGeo, boxMat);
+
+scene.add(box);
+
+const boxBody = new CANNON.Body({
+  shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1)),
+  mass: 1,
+  position: new CANNON.Vec3(1, 20, 0)
+})
+
+world.addBody(boxBody)
+
+const timeStep = 1 / 60;
 
 function animate() {
+  world.step(timeStep);
+
+  plane.position.copy(groundBody.position)
+  plane.quaternion.copy(groundBody.quaternion)
+
+  box.position.copy(boxBody.position)
+  box.quaternion.copy(boxBody.quaternion)
 
   renderer.render(scene, camera);
 }
