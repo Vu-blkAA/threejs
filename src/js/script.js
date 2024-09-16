a6ffe9d31a3f96707b61f2c8e2c47d8aca4edcb4
Vu-blkaa <vublkaa@gmail.com>
2024-09-16 08:15:09 +0700

    [video6] Complete video 6

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-import stars from "../image/stars.jpg";
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -10,32 +8,57 @@ document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
+  45,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
-camera.position.set(1, 0, 0)
+camera.position.set(0, 6, 2);
 
 const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.update();
 
-const gridHelper = new THREE.GridHelper(30);
-scene.add(gridHelper);
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
 
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-scene.background = cubeTextureLoader.load([
-  stars,
-  stars,
-  stars,
-  stars,
-  stars,
-  stars,
-]);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+scene.add(directionalLight);
+
+directionalLight.position.set(0, 50, 0);
+
+const helper = new THREE.AxesHelper(20);
+// scene.add(helper);
+
+const mouse = new THREE.Vector2();
+const intersectionPoint = new THREE.Vector3();
+const planeNormal = new THREE.Vector3();
+const plane = new THREE.Plane();
+const rayCaster = new THREE.Raycaster();
+
+window.addEventListener("mousemove", (e) => {
+  mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+  console.log(mouse.x, mouse.y)
+  planeNormal.copy(camera.position).normalize();
+  plane.setFromNormalAndCoplanarPoint(planeNormal, scene.position);
+  rayCaster.setFromCamera(mouse, camera);
+  rayCaster.ray.intersectPlane(plane, intersectionPoint);
+});
+
+window.addEventListener("click", (e) => {
+  const sphereGeo = new THREE.SphereGeometry(0.125, 30, 30);
+  const sphereMat = new THREE.MeshStandardMaterial({
+    color: 0xffea00,
+    metalness: 0,
+    roughness: 0,
+  });
+  const sphere = new THREE.Mesh(sphereGeo, sphereMat);
+  scene.add(sphere);
+  sphere.position.copy(intersectionPoint)
+});
 
 function animate() {
-
   renderer.render(scene, camera);
 }
 
